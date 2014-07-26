5d6b026f6d7a3b0b2439407d37dfb7d671e4ad97
Mani Bhushan <manibhushan.cs@gmail.com>
2014-07-26 08:37:47 +0530

    calculate inverse of square matrix with caching support - PA2 for rprog-005

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a matrix object with get and set apis for the matrix itself and
+## it has getMatrixInv & setMatrixInv to access the inverse of the matrix (if possible)
+## --Its assumed the matrix would always be square matrix but its not assumed that the 
+## determinant of the matrix would always be NON-ZERO!
 
-## Write a short comment describing this function
+## creates the matrix object and exposes the getter/setter functions for matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-## test
+    minv <- NULL
+
+    set <- function(y) {
+        x <<- y
+        minv <<- NULL
+    }
+
+    get <- function() x
+
+    setMatrixInv <- function(inv) minv <<- inv
+    getMatrixInv <- function() minv
+
+    list(set=set, get=get, getMatrixInv=getMatrixInv, setMatrixInv=setMatrixInv)
 }
 
 
-## Write a short comment describing this function
+## calculates the inverse (if det(matrix) != 0) of the square matrix and caches the results.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        minv <- x$getMatrixInv()
+        if (!is.null(minv)) {
+            message("getting cached matrix inverse")
+            return(minv)
+        }
+        mat <- x$get()
+        mdet <- det(mat)
+        mdet <- round(mdet,3)
+        if (mdet == 0) {
+            message("determinant of the matrix is ZERO, hence no inverse possible")
+            minv <- NULL
+        } else {
+            minv <- solve(mat)
+        }
+        x$setMatrixInv(minv)
+        return(minv)
 }
